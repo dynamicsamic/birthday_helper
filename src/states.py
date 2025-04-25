92916ec1115d916669f7499a75bbe7152e649f47
dynamicsamic <mirabyansr@gmail.com>
2025-04-26 01:20:20 +0300

    refactor: remove duplicate code remained from ver1

--- src/states.py
+++
@@ -1,8 +0,0 @@
-from aiogram.dispatcher.filters.state import State, StatesGroup
-
-
-class AddBirthday(StatesGroup):
-    month = State()
-    day = State()
-    name = State()
-    complete = State()
