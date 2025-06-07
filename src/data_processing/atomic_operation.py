3c8d80fbc0080977df1488c2be59ee6cc5a833a2
dynamicsamic <mirabyansr@gmail.com>
2025-06-07 12:03:55 +0300

    refactor: update code to eliminate type annotations mistakes

--- src/data_processing/atomic_operation.py
+++ src/data_processing/atomic_operation.py
@@ -1,46 +1,52 @@
 import asyncio
-from typing import Protocol
 from pathlib import Path
+from typing import Protocol, final
+
 from src.logging import get_logger
+from src.exceptions import AtomicOperationError
 
 logger = get_logger(__name__)
 
 
 class WriteSafeFileHandlerProtocol(Protocol):
-    def get_lock(self) -> asyncio.Lock:
-        ...
+    def get_lock(self) -> asyncio.Lock: ...
 
-    async def create_backup(self) -> Path:
-        ...
+    async def create_backup(self) -> Path: ...
 
-    async def restore_backup(self, backup_path: Path):
-        ...
+    async def restore_backup(self, backup_path: Path): ...
 
 
+@final
 class AtomicOperation:
     def __init__(self, handler: WriteSafeFileHandlerProtocol):
         self.handler = handler
         self.lock = self.handler.get_lock()
         self.backup_path: Path | None = None
+        self._lock_acquired: bool = False
 
     async def __aenter__(self):
-        logger.debug("Acquiring write lock for atomic write")
-        await self.lock.acquire()
-        self.backup_path = await self.handler.create_backup()
-        # return self.handler
+        if not self._lock_acquired:
+            logger.debug("Acquiring write lock for atomic write")
+            self._lock_acquired = await self.lock.acquire()
+            self.backup_path = await self.handler.create_backup()
 
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
-        if self.lock.locked():
+    async def __aexit__(
+        self, exc_type: type[BaseException] | None, exc_val: BaseException | None, _
+    ):
+        if self._lock_acquired:
             logger.debug("Releasing write lock for atomic write")
             self.lock.release()
+            self._lock_acquired = False
 
         if exc_type is not None:
             logger.error(
-                f"{exc_type} exception with value {exc_val} caught during atomic write.\n"
-                f"Restoring backup from {self.backup_path}"
+                (
+                    f"{exc_type} exception with value {exc_val} caught during atomic write.\n"
+                    f"Restoring backup from {self.backup_path}"
+                )
             )
 
             if self.backup_path:
                 await self.handler.restore_backup(self.backup_path)
 
-            raise exc_val
+            raise AtomicOperationError(exc_val)
